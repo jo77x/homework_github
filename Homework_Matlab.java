d5a3590506da3f88e5514de3f60131e7334b30d3
Johannes <you@example.com>
2025-11-16 18:35:26 +0100

    Version mit J&C

---
+++ Homework_Matlab.java
@@ -0,0 +1,10 @@
+public class Homework_Matlab{
+
+   public static void main (String[] args) {
+   String name = "Johannes";
+   System.out.println(name);
+   
+   String name_2 = "Corina";
+   System.out.println(name_2);
+   }
+}
